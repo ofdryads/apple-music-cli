09692b6aaf24f986d833243cf3902d67a950a169
ofdryads <71109631+ofdryads@users.noreply.github.com>
2025-08-31 13:57:04 -0400

    back command

---
+++ cmd/back.go
@@ -0,0 +1,45 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/andybrewer/mack"
+	"github.com/spf13/cobra"
+)
+
+var backCmd = &cobra.Command{
+	Use:   "back",
+	Short: "Go back to the previous track in Apple Music",
+	Run: func(cmd *cobra.Command, args []string) {
+		isOpen, err := isMusicOpen()
+		if err != nil {
+			fmt.Println("Error checking if Apple Music is open:", err)
+			return
+		}
+
+		if !isOpen {
+			fmt.Println("Apple Music is not open!")
+			return
+		}
+
+		if _, err := mack.Tell("Music", "previous track"); err != nil {
+			fmt.Println("Error skipping the song:", err)
+		}
+
+		info, err := getCurrentSongInfo()
+		if err != nil {
+			fmt.Printf("Error getting current song info: %v", err)
+			return
+		}
+
+		if info.trackName == "" {
+			fmt.Println("Song Skipped")
+			return
+		}
+		fmt.Printf("Now Playing: %s\nBy: %s\n", info.trackName, info.artistName)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(backCmd)
+}
